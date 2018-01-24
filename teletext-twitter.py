dabbe871a4d599c6854cd0a3393573bba28f91bf
mpentler <tehhustler@hotmail.com>
2018-01-24 13:19:12 +0000

    Added argparse code with some options including quiet mode, error checking added

--- teletext-twitter.py
+++ teletext-twitter.py
@@ -1,32 +1,28 @@
 # teletext-twitter - creates pages for vbit2 teletext system
 # (c) Mark Pentler 2018 (https://github.com/mpentler)
-# see README.md for details on getting it running
+# see README.md for details on getting it running or run with -h
 
 import twitter
 import time
 import sys
 import textwrap
-
-# Read config.py for our access keys etc
-config = {}
-exec(open("config.py").read(), config)
-twitter = twitter.Api(access_token_key = config["access_key"],
-                      access_token_secret = config["access_secret"],
-                      consumer_key = config["consumer_key"],
-                      consumer_secret = config["consumer_secret"],
-                      sleep_on_rate_limit = True) # so we don't hit the rate limit and raise an exception
+import argparse
 
 # define our control codes here for easy use later
 ESCAPE = chr(27)
 RED = ESCAPE + chr(65)
-GREEN = ESCAPE + chr(66)
-YELLOW = ESCAPE + chr(67)
-BLUE = ESCAPE + chr(68)
-MAGENTA = ESCAPE + chr(69)
 CYAN = ESCAPE + chr(70)
-WHITE = ESCAPE + chr(71)
 DOUBLE_HEIGHT = ESCAPE + chr(77)
-SINGLE_HEIGHT = ESCAPE + chr(76)
+MOSAIC_CYAN = ESCAPE + chr(86)
+
+# Read config.py for our Twitter access keys etc
+config = {}
+exec(open("config.py").read(), config)
+twitter_object = twitter.Api(access_token_key = config["access_key"],
+                      access_token_secret = config["access_secret"],
+                      consumer_key = config["consumer_key"],
+                      consumer_secret = config["consumer_secret"],
+                      sleep_on_rate_limit = True) # so we don't hit the rate limit and raise an exception
 
 def write_header(): # write a header for the page and pop a nice banner at the top
     with open("/home/pi/teletext/P153.tti", "w+") as file:
@@ -34,11 +30,11 @@ def write_header(): # write a header for the page and pop a nice banner at the t
         file.write("PN,15300\r\n")
         file.write("SC,0000\r\n")
         file.write("PS,8000\r\n")
-        file.write("OL,1," + DOUBLE_HEIGHT + "TELETEXT TWITTER" + chr(27) + chr(86) + (chr(127) * 22) + "\r\n")
-        file.write("OL,3," + chr(27) + chr(86) + (chr(35) * 39) + "\r\n")
+        file.write("OL,1," + DOUBLE_HEIGHT + "TELETEXT TWITTER" + MOSAIC_CYAN + (chr(127) * 22) + "\r\n")
+        file.write("OL,3," + MOSAIC_CYAN + (chr(35) * 39) + "\r\n")
 
 def write_timeline(): # grab the latest timeline - only 5 tweets for now
-    statuses = twitter.GetHomeTimeline(count = 5)
+    statuses = twitter_object.GetHomeTimeline(count = 5)
     line_position = 4
 
     for status in statuses: # iterate through our responses
@@ -54,26 +50,46 @@ def write_timeline(): # grab the latest timeline - only 5 tweets for now
             file.write("OL,{},".format(str(line_position)) + ("`" * (36-len(tweet_human_time)-len(tweet_username))) + "@{}".format(tweet_username) + " | " + "{}".format(tweet_human_time) + "\r\n")
             line_position += 1
             for line in tweet_text:
-                line = line.replace("’", "'")
-                line = line.replace("_", "-")
-                line = line.replace("#", "_")
+                line = line.replace("’", "'") # replacing some problematic characters here
+                line = line.replace("_", "-") # the teletext English character set doesn't
+                line = line.replace("#", "_") # support a lot of things!
                 file.write("OL,{},".format(str(line_position)) + RED + "{}\r\n".format(line))
                 line_position += 1
 
-def main():
-    print("[*] teletext-twitter - (c) 2018 Mark Pentler (https://github.com/mpentler)", file=sys.stdout)
-    print("[*] Beginning timeline scraping", file=sys.stdout)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Reads your Twitter timeline and turns it into teletext pages for your Raspberry Pi.")
+    parser.add_argument("-d", "--delay", type=int, default=60, help="seconds between timeline scrapes (minimum is 60 seconds - lower values have no effect)")
+    parser.add_argument("-v", "--version", action="version", version="0.1")
+    parser.add_argument("-q", "--quiet", dest="quiet", action="store_true", default=False, help="suppresses all output to the terminal except warnings and errors")
+    args = parser.parse_args()
+    args.delay = max(60, args.delay)
+    return args
 
-    # for now we'll update for 60 seconds - may want to make this changeable?
+def main():
+    args = parse_args()
+
+    if not args.quiet:
+        print("[*] teletext-twitter - (c) 2018 Mark Pentler (https://github.com/mpentler)", file=sys.stdout)
+        print("[*] Beginning timeline scraping", file=sys.stdout)
+
     while True:
-        write_header()
-        write_timeline()
-        print("[*] Waiting 60 seconds until next scrape", file=sys.stdout)
-        time.sleep(60)
+        try:
+            write_header()
+            write_timeline()
+            if not args.quiet:
+                print("[*] Page updated. Waiting {} seconds until next scrape".format(args.delay), file=sys.stdout)
+        except OSError as e:
+            print("[!] Error accessing teletext data file, exiting: {}".format(e.strerror), file=sys.stderr)
+            sys.exit(1)
+        except twitter.error.TwitterError as e:
+            print("[!] Error accessing your Twitter timeline: {}".format(e), file=sys.stderr)
+            print("[!] Trying again after specified delay", file=sys.stderr)
+        else:
+            time.sleep(args.delay)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("[!] Interrupted by user. Exiting...", file=sys.stdout)
+        print("[*] Interrupted by user. Exiting...", file=sys.stdout)
         sys.exit(0)
